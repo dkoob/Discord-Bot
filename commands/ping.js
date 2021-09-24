4c1b60caa464a694aa319ff2e77a7f0ad942f1bf
Dylan K <dkooby@Dylans-MacBook-Pro.local>
2021-09-24 10:35:59 -0400

    Added comments

--- commands/ping.js
+++ commands/ping.js
@@ -4,6 +4,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 // states if the slash command conatains ping it will reply with the word 'pong!'
 module.exports = {
 	data: new SlashCommandBuilder()
+		
+	// actual body of the command, everything above ATLEAST is required for every command
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
