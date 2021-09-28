104c421cc141796128eab4d2b5488cf26bd97fb1
Dylan K <dkooby@Dylans-MacBook-Pro.local>
2021-09-28 10:36:27 -0400

    Added embed to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,12 @@
 // requires discord slashcommandbuilder constant
+const { MessageEmbed } = require('discord.js')
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const pingembed = new MessageEmbed()
+        .setDescription("Pong!")
+        .setColor("RANDOM")
+        .setTimestamp()
+
 
 // states if the slash command conatains ping it will reply with the word 'pong!'
 module.exports = {
@@ -8,7 +15,8 @@ module.exports = {
 	// actual body of the command, everything above ATLEAST is required for every command
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
+
 	async execute(client, interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply({embeds : [pingembed]});
 	},
 }; 
